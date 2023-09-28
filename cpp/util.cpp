798376772e845b396906665cc0c17962e04522ba
Michael Butler <mbutler@tumblr.com>
2023-09-28 17:59:14 -0400

    Remove Cpp client

--- cpp/util.cpp
+++ cpp/util.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <string>
+#include "defines.h"
 #include "util.h"
 
 using namespace std;
@@ -34,4 +36,50 @@ void debuglog(string log)
         return;
     }
     cout << log << endl;
+}
+
+// return TRUE (0) if valid or FALSE (0) if not
+int is_valid_message(string msg)
+{
+    char* bytes;
+    string work_buffer;
+
+    if (msg.length() < 3) {
+        debuglog("Message is under 3 chars long");
+        return FALSE;
+    }
+
+    bytes = msg.data();
+
+    if (
+        bytes[0] != 'J' &&
+        bytes[0] != 'C' &&
+        bytes[0] != 'L' &&
+        bytes[0] != 'U' &&
+        bytes[0] != 'M' &&
+        bytes[0] != 'Q' &&
+        bytes[0] != 'V'
+    ) {
+        debuglog("Message does not begin with proper char");
+        return FALSE;
+    }
+
+    // for now just require two zeroes.
+    if (bytes[1] != '0' || bytes[2] != '0') {
+        debuglog("Message does not have 00 after first char");
+        return FALSE;
+    }
+
+    // Validate Join Command
+    if (bytes[0] == 'J') {
+        work_buffer = msg.substr(3);
+        if (!is_valid_entity_name(work_buffer)) {
+            debuglog("Invalid entity name provided.");
+            return FALSE;
+        }
+    }
+
+    debuglog("Message is valid. Proceeding.");
+
+    return TRUE;
 }
